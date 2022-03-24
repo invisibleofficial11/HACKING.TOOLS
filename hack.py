9f2ae2fa86917112f98b4d84f7e5241f931aaa2a
GOD INVISIBLE <101255902+invisibleofficial11@users.noreply.github.com>
2022-03-24 09:21:40 -0700

    Create hack.py

---
+++ hack.py
@@ -0,0 +1,8 @@
+ytstfj input java 
+input java await;
+await; java python php hack linux shell
+FROM input (hacking")'hack.send))str{in}
+            hack = legendhacker_iin}
+        {
+          ]
+          
