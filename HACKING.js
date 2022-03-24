dbf6fe764696057ef8460af8b117dbb0d2f741c6
GOD INVISIBLE <101255902+invisibleofficial11@users.noreply.github.com>
2022-03-24 09:19:52 -0700

    Update HACKING.js

--- HACKING.js
+++ HACKING.js
@@ -1,1 +1,29 @@
 
+synsuc Python FROM git. MR-INVISIVLEBOY
+RUN Legendhack
+ print(hack)"
+await:(hacklegend.invisivle/
+       await:hacking
+     await:'hack"
+                      LEGEND=("INPUT(JAVA")HACK.EDIT.SEND./?
+  PRINT(:JAVA""")()()))
+        PRINT(A:).SEND
+JAVA SCRIPT (HACKING USER ID STRING SEND 2SET)'"'"''
+
+input("python,java,linux.)())
+      send.messege.5267090352.)))
+user= @mr_invisivle_official))
+]
+{
+                            hacking kess ><java,php./?=uythjsJLJuBhmjdghKHHJHJDH767894NBXJFBVBKKXIFNKLEGENDHACKER}
+{
+  
+  {}SEND.STRING USERID in str(input(in await;
+                                    wait ;
+                            await in output send(5267090352.)))"""
+  java (print{str{a}}}
+  in await:
+  
+  
+  await it in input .hacking......
+  
